8481b98e2388569b4dc27a19a584ebd42f288201
Kulsoom-1 <kulsoom.maavan@gmail.com>
2019-11-09 16:20:03 +0500

    navigation chanegs

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Error from'./pages/Error';
 import Rooms from'./pages/Rooms';
 import SingleRoom from'./pages/SingleRoom';
 import Navbar from './components/Navbar';
-import {Route,Switch} from 'react-router-dom';
+import {Route,Switch,  BrowserRouter as Router,} from 'react-router-dom';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import FeaturedRooms from './components/FeaturedRooms';
@@ -14,21 +14,8 @@ import FeaturedRooms from './components/FeaturedRooms';
 function App() {
   return (
         <div>
-             <Navbar>
-              
-                  <Switch>
-                    <Route exact path="/" component={Home}/>
-                     <Route exact  path="/rooms" component={Rooms}/>
-                     <Route exact path="/single-room:slug" component={SingleRoom}/>
-                     <Route  component={Error}/>
-                     
-                 </Switch>
-           </Navbar>
-           <Hero> </Hero>
-           <Services></Services>
-          <FeaturedRooms></FeaturedRooms>
-         
-          
+            <Navbar />
+            <Home />
         </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import logo from '../images/logo.svg';
 import {FaAlignRight} from 'react-icons/fa';
-import {Link} from 'react-router-dom';
-
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import Home from'../pages/Home';
+import Error from'../pages/Error';
+import Rooms from'../pages/Rooms';
 class Navbar extends Component{
 
     state={
@@ -14,35 +22,45 @@ class Navbar extends Component{
     }
     render(){
         return(
-          <nav className="navbar">
-                     <div className="nav-center">
-                     <div className="nav-header">
-                         
-                            <Link to="/">
-                                <img src={logo} alt="Beach Resort" />
-                            </Link>
-               <button 
-                    type="button"
-                       className="nav-btn"
-                          onClick={this.handleToggle}>
-                              <FaAlignRight className="nav-icon" />
-                </button>
-               
-                 </div>
-                           <ul className={this.state.isOpen?"nav-links show-nav": "nav-links"}>
-                                   <li>
-                                      <Link to="/">Home</Link>
-                                </li>
-                                      <li> 
-                                        <Link to="/">Rooms</Link>
-                                      </li>
-                    
-                                </ul> 
-                </div>
-            </nav>
-           
+        <Router>
+          <div>
+           <nav className="navbar">
+             <div className="nav-center">
+               <div className="nav-header">
+                 <Link to="/">
+                      <img src={logo} alt="Beach Resort" />
+                 </Link>
+                 <button
+                  type="button"
+                  className="nav-btn"
+                  onClick={this.handleToggle}>
+                  <FaAlignRight className="nav-icon" />
+                  </button>
+               </div>
+               <ul className={this.state.isOpen?"nav-links show-nav": "nav-links"}>
+                 <li>
+                    <Link to="/home">Home</Link>
+                 </li>
+                 <li>
+                    <Link to="/rooms">Rooms</Link>
+                </li>
+              </ul>
+             </div>
+           </nav>
+           <Switch>
+              <Route path="/home">
+                  <Home />
+              </Route>
+              <Route path="/rooms">
+                  <Rooms />
+              </Route>
+              <Route>
+                  <Error />
+              </Route>
+           </Switch>
+          </div>
+        </Router>
   )
-        
-    }
+  }
 }
 export default Navbar;
